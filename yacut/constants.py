9c3abe163bc8fe0df8f511e3bbea842ffc9c6921
Levon Gegamyan <levongeg@gmail.com>
2023-03-19 00:35:39 +0300

    Added templated index page, URLModel model, and LinksForm form

---
+++ yacut/constants.py
@@ -0,0 +1,4 @@
+import string
+
+CHARACTERS = string.ascii_letters + string.digits
+LINK_LENGHT = 6
---
+++ yacut/views.py
@@ -0,0 +1,10 @@
+from flask import render_template
+
+from . import app
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index_view():
+    return render_template('index.html')
+
+
